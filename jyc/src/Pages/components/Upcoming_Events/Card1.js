d0079a9047bd05462549152de952cce328985e09
Mayank Kumar <211317@juitsolan.in>
2022-02-26 10:07:31 +0530

    fixed everything except header and footer

--- jyc/src/Pages/components/Upcoming_Events/Card1.js
+++ jyc/src/Pages/components/Upcoming_Events/Card1.js
@@ -12,21 +12,21 @@ export default function Card1() {
                     <Eventcard                  
                     img={image}
                     heading='QUALIFA'
-                    date='2022'
+                    // date='2022'
                     about='The virtual edition of JYC first-ever literary , theatrical and musical events.'/>
                 </div>
                 <div className='card-ue'>
                     <Eventcard
                     img={image2}
                     heading='WELLNESS WEEK'
-                    date='2022'
+                    // date='2022'
                     about='Unite people through common goals of community awareness, prompting a better quality of life'/>
                 </div>
                 <div className='card-ue'>
                     <Eventcard
                     img={image3}
                     heading='ATKHELIYAN'
-                    date='2022'
+                    // date='2022'
                     about='From showing best dance moves to dressing up in the best attires, a chance to showcase talent with Bollywood tadka.
                     '/>
                 </div>
--- jyc/src/Pages/components/Upcoming_Events/Heading.js
+++ jyc/src/Pages/components/Upcoming_Events/Heading.js
@@ -5,7 +5,7 @@ import './Heading.css'
 export default function Heading() {
   return (
   
-      <div className='heading'>UPCOMING EVENTS
+      <div className='heading'>Upcoming Eevents
     </div>
   
   )
